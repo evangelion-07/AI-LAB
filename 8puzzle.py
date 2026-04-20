f89d6cf327eda7f85dc5e9c2dc94eb85271c5873
fangyuan07 <satheeshannarapu@gmail.com>
2026-04-20 09:19:28 +0530

    Implement BFS for 8-puzzle problem
    Replaced backtracking solution with BFS for 8-puzzle problem. Added functions to print the board and find neighbors.

--- 8puzzle.py
+++ 8puzzle.py
@@ -1,17 +1,43 @@
-count = 1
-board = ["_"*8 for _ in range(8)]
-def backtrack(r):
-    global count
-    if r == 8:
-        print("Solution", count)
-        count += 1
-        for row in board:
-            print(" ".join(row))
-        print()
-        return
-    for c in range(8):
-        if all(board[i][c]=="_" and abs(c-board[i].find("Q"))!=r-i for i in range(r)):
-            board[r] = board[r][:c] + "Q" + board[r][c+1:]
-            backtrack(r+1)
-            board[r] = "_"*8
-backtrack(0)
++from collections import deque
+
+goal = '123456780'
+
+
+def print_board(s):
+    for i in range(0, 9, 3):
+        print(' '.join(s[i:i+3]))
+    print()
+
+
+def neighbors(s):
+    i = s.index('0')
+    moves = []
+    swap = [(1,3),(0,2,4),(1,5),(0,4,6),(1,3,5,7),(2,4,8),(3,7),(4,6,8),(5,7)]
+    for j in swap[i]:
+        lst = list(s)
+        lst[i], lst[j] = lst[j], lst[i]
+        moves.append(''.join(lst))
+    return moves
+
+
+def bfs(start):
+    q = deque([(start, [start])])  
+    seen = set([start])
+    
+    while q:
+        state, path = q.popleft()
+        
+        if state == goal:
+            print(f"Steps: {len(path)-1}\n")
+            print("Solution Path:\n")
+            for step in path:
+                print_board(step)
+            return
+        
+        for n in neighbors(state):
+            if n not in seen:
+                seen.add(n)
+                q.append((n, path + [n]))
+
+
+bfs('461253078')
